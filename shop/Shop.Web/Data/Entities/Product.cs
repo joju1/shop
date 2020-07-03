611732ff7e544a08dba0a911ac4f08f54b82894d
joju <joju@LAPTOP-2FL7FOSK>
2020-07-03 19:35:58 -0400

    Modify Db, Seeder

--- shop/Shop.Web/Data/Entities/Product.cs
+++ shop/Shop.Web/Data/Entities/Product.cs
@@ -6,6 +6,9 @@ namespace Shop.Web.Data.Entities
     public class Product
     {
         public int Id { set; get; }
+
+        [MaxLength(50,ErrorMessage ="The field {0} just need a maximun {1} characters")]
+        [Required]
         public string Name { set; get; }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode =false)]
@@ -14,10 +17,10 @@ namespace Shop.Web.Data.Entities
         [Display(Name = "Image")]
         public string ImageURL { set; get; }
         [Display(Name ="Last Sale")]
-        public DateTime LastSale { set; get; }
+        public DateTime? LastSale { set; get; }
 
         [Display(Name ="Last Purchase")]
-        public DateTime LastPurchase { set; get; }
+        public DateTime? LastPurchase { set; get; }
 
         [Display(Name ="Is Avialable?")]
         public bool IsAvialable { set; get; }
---
+++ shop/Shop.Web/Data/SeedDb.cs
@@ -0,0 +1,44 @@
+﻿
+namespace Shop.Web.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Entities;
+    public class SeedDb
+    {
+        private readonly DataContext context;
+        private Random random;
+
+        public SeedDb(DataContext context)
+        {
+            this.context = context;
+            this.random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            await this.context.Database.EnsureCreatedAsync();
+
+            if(!this.context.Products.Any())
+            {
+                this.AddProduct("Iphone");
+                this.AddProduct("Huawei");
+                this.AddProduct("IPad");
+
+                await this.context.SaveChangesAsync();
+            }
+        }
+
+        private void AddProduct(string name)
+        {
+            this.context.Products.Add(new Product
+            {
+                Name = name,
+                Price = this.random.Next(2000),
+                Stock = this.random.Next(50)
+            });
+        }
+    }
+}
